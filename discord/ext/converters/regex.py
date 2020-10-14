6b8d82badd48830d4ef23639421c79ca2d605789
Jay Turner <jay@trainerdex.co.uk>
2020-10-14 18:41:52 +0100

    Apply suggestions from code review to regex converter
    * Pass flags to re.fullmatch
    * Use typing.Optional rather than typing.Union[..., None]
    * Use re.fullmatch rather than re.match
    * Return the match object rather than str
    
    Co-authored-by: James <50501825+Gobot1234@users.noreply.github.com>
    Co-authored-by: Riley Shaw <30989490+ShineyDev@users.noreply.github.com>

--- discord/ext/converters/regex.py
+++ discord/ext/converters/regex.py
@@ -1,16 +1,22 @@
 import re
-from typing import Union
+from typing import Optional
 
 from discord.ext import commands
 
 
 class MatchConverter(commands.Converter):
-    def __init__(self, *, pattern: str, exception: str = "No match found.") -> None:
+    def __init__(self, pattern, flags) -> None:
         self.pattern = pattern
-        self.exception = exception
+        self.flags = flags
 
-    async def convert(self, ctx, argument: str) -> str:
-        match: Union[re.Match, None] = re.match(self.pattern, argument)
-        if match is None:
-            raise commands.BadArgument(self.exception)
-        return argument
+    async def convert(self, ctx, argument: str) -> re.Match:
+        match: Optional[re.Match] = re.fullmatch(
+            self.pattern, argument, flags=self.flags
+        )
+        if match:
+            raise commands.BadArgument(
+                "{0} does not match in the regex pattern ({1})".format(
+                    argument, self.pattern
+                )
+            )
+        return match
