d9f97499794f1612b7ca532bc2f8dcc6ab4da8c4
Jay Turner <jaynicholasturner@gmail.com>
2020-10-14 18:25:18 +0100

    Apply suggestions from code review to range converter
    * Pass args and kwargs from InRangeConverter to the range object being created. (Thanks @Gobot1234 !)
    * Convert argument to a int before checking it's in the range (Thanks @ShineyDev !)
    
    Co-authored-by: James <50501825+Gobot1234@users.noreply.github.com>
    Co-authored-by: Riley Shaw <30989490+ShineyDev@users.noreply.github.com>

--- discord/ext/converters/range.py
+++ discord/ext/converters/range.py
@@ -4,10 +4,10 @@ from discord.ext import commands
 
 
 class InRangeConverter(commands.Converter):
-    def __init__(self, *, range_tuple: Tuple) -> None:
-        self.range = range(*range_tuple)
+    def __init__(self, *args, **kwargs) -> None:
+        self.range = range(*args, **kwargs)
 
-    async def convert(self, ctx, argument: str) -> str:
-        if argument in self.range:
-            return argument
-        raise commands.BadArgument("{argument} not in range".format(argument=argument))
+    async def convert(self, ctx, argument: str) -> int:
+        if argument.isnumeric() and int(argument) in self.range:
+            return int(argument)
+        raise commands.BadArgument("{0} not in range".format(argument))
