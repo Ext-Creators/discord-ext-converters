4701cef3b76e528349684ed5adb0e8d7b90d9ad7
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2021-03-03 14:33:49 +0000

    update setup

--- setup.py
+++ setup.py
@@ -1,44 +1,46 @@
-from setuptools import setup
 import re
+import setuptools
+
 
 with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+    install_requires = f.read().splitlines()
 
-version = ""
-with open("discord/ext/converters/__init__.py") as f:
-    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", f.read(), re.MULTILINE).group(1)
+with open("discord/ext/converters/__init__.py", "r") as stream:
+    version = re.search(r"^version = [\"]([^\"]*)[\"]", stream.read(), re.MULTILINE).group(1)
 
-with open("README.rst", encoding="utf-8") as f:
-    readme = f.read()
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
 
-setup(
-    name="discord-ext-converters",
+project_urls = {
+    "Issue Tracker": "https://github.com/Ext-Creators/discord-ext-converters/issues",
+    "Source": "https://github.com/Ext-Creators/discord-ext-converters",
+}
+
+setuptools.setup(
     author="Ext-Creators",
-    python_requires=">=3.6",
+    classifiers=classifiers,
+    description="A discord.py extension with a collection of useful converters.",
+    install_requires=install_requires,
+    license="Apache Software License",
+    name="discord-ext-converters",
+    packages=["discord.ext.converters", "discord.ext.converters.custom_converters"],
+    project_urls=project_urls,
+    python_requires=">=3.6.0",
     url="https://github.com/Ext-Creators/discord-ext-converters",
     version=version,
-    packages=["discord.ext.converters", "discord.ext.converters.custom_converters"],
-    license="Apache Software License",
-    description="A collection of useful converters.",
-    long_description=readme,
-    long_description_content_type="text/x-rst",
-    include_package_data=True,
-    install_requires=requirements,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Internet",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-    ]
 )
