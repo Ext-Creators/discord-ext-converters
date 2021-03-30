e3c92082913717f8b9b0921b5d95abdff4e9d0e0
James <50501825+Gobot1234@users.noreply.github.com>
2021-03-30 12:31:20 +0100

    Update converter.py

--- discord/ext/converters/converter.py
+++ discord/ext/converters/converter.py
@@ -1,22 +1,12 @@
 import typing
 from discord.ext import commands
 import copy
-import sys
 
-if sys.version_info >= (3, 7):
-    class CustomConverter(commands.Converter):
-        _value: typing.Any
+class CustomConveterMeta(type):
+    def __getitem__(cls, value: typing.Any) -> 'CustomConverter':
+        klass = copy.deepcopy(cls)
+        klass._value = value
+        return klass
 
-        def __class_getitem__(cls, value: typing.Any) -> 'CustomConverter':
-            klass = copy.deepcopy(cls)
-            klass._value = value
-            return klass
-else:
-    class CustomConveterMeta(type):
-        def __getitem__(cls, value: typing.Any) -> 'CustomConverter':
-            klass = copy.deepcopy(cls)
-            klass._value = value
-            return klass
-        
-    class CustomConverter(commands.Converter, metaclass=CustomConveterMeta):
-        _value: typing.Any
+class CustomConverter(commands.Converter, metaclass=CustomConveterMeta):
+    _value: typing.Any
